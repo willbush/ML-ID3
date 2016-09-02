866de9c33623d5c0aff623483de385a860fa1383
willbush <william.bush@utdallas.edu>
2016-09-02 00:35:42 -0500

    add entropy calc fix file parsing to handle whitespace

--- src/Program.java
+++ src/Program.java
@@ -1,17 +1,42 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Program {
+    private static final String WHITE_SPACE_REGEX = "\\s+";
+
     public static void main(String[] args) {
         if (args.length != 2) {
             printProgramUsage();
         }
 
         try {
-            printFileContents(args[0]);
-            printFileContents(args[1]);
+            System.out.println(getSetFromFileAsString(args[0]));
+            System.out.println(getSetFromFileAsString(args[1]));
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    static String getSetFromFileAsString(String path) {
+        return convertSetToString(getSetFromFile(path));
+    }
+
+    static String convertSetToString(List<Boolean[]> set) {
+        StringBuilder sb = new StringBuilder();
+
+        for (Boolean[] elements : set) {
+            for (int i = 0; i < elements.length; ++i) {
+                sb.append(elements[i] ? "1" : "0");
+                if (i != elements.length - 1)
+                    sb.append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     private static void printProgramUsage() {
@@ -22,15 +47,52 @@ public class Program {
         System.out.println(usage);
     }
 
-    public static void printFileContents(String path) {
+    private static List<Boolean[]> getSetFromFile(String path) {
+        List<Boolean[]> result = new LinkedList<>();
         File f = new File(path);
+
         try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+            readClassAttributes(br);
             String line;
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
+                if (!line.isEmpty()) {
+                    String[] words = line.split(WHITE_SPACE_REGEX);
+                    Boolean[] row = new Boolean[words.length];
+                    for (int i = 0; i < words.length; ++i) {
+                        row[i] = words[i].equals("1");
+                    }
+                    result.add(row);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return result;
+    }
+
+    private static void readClassAttributes(BufferedReader br) throws IOException {
+        String line;
+        while ((line = br.readLine()) != null && line.isEmpty()) {
+            //TODO set attributes.
+        }
+    }
+
+    static double calcEntropy(int classACount, int classBCount) {
+        final int setTotalCount = classACount + classBCount;
+        final double probabilityA = (double) classACount / setTotalCount;
+        final double probabilityB = (double) classBCount / setTotalCount;
+
+        final double A = (classACount == 0) ? 0.0 : probabilityA * log2(probabilityA);
+        final double B = (classBCount == 0) ? 0.0 : probabilityB * log2(probabilityB);
+
+        return -A - B;
+    }
+
+    private static double log2(double d) {
+        return Math.log(d) / Math.log(2);
+    }
+
+    static double calcInfoGain(List<Boolean[]> set) {
+        return 0;
     }
 }
--- test/ProgramTest.java
+++ test/ProgramTest.java
@@ -1,11 +1,70 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class ProgramTest {
+    private static final String SPAM_EXAMPLE = "1 0 0 1\n" +
+            "0 0 1 1\n" +
+            "0 0 0 0\n" +
+            "1 1 0 0\n" +
+            "0 0 0 0\n" +
+            "1 0 1 1\n" +
+            "0 1 1 0\n" +
+            "1 0 0 1\n";
+
     @Test
-    public void canPrintTestData() {
-        Program.main(new String[]{"resources/dataSet1/train.dat", "resources/dataSet1/test.dat"});
-        assertTrue(true);
+    public void canCorrectlyParseFileWithExtraWhiteSpace() {
+        final String dataFormatTest = "resources/dataFormatTest/test.dat";
+        final String expected = "0 0 0 0\n" +
+                "0 0 1 0\n" +
+                "0 1 0 0\n" +
+                "0 1 1 0\n" +
+                "1 0 0 0\n" +
+                "1 0 1 0\n" +
+                "1 1 0 0\n" +
+                "1 1 1 1\n";
+        assertEquals(expected, Program.getSetFromFileAsString(dataFormatTest));
+    }
+
+    @Test
+    public void canGetWhatWasGiven() {
+        assertEquals(SPAM_EXAMPLE, Program.convertSetToString(setFromString(SPAM_EXAMPLE)));
+    }
+
+    @Test
+    public void canCalculateEntropy() {
+        assertEquals(0.0, Program.calcEntropy(0, 5), 0.01);
+        assertEquals(0.0, Program.calcEntropy(5, 0), 0.01);
+
+        assertEquals(1.0, Program.calcEntropy(2, 2), 0.001);
+
+        assertEquals(0.59, Program.calcEntropy(1, 6), 0.01);
+        assertEquals(0.97, Program.calcEntropy(3, 2), 0.01);
+        assertEquals(0.918, Program.calcEntropy(1, 2), 0.001);
+    }
+
+    @Test
+    public void canCalculateInformationGain() {
+        assertEquals(0.0, Program.calcInfoGain(setFromString(SPAM_EXAMPLE)), 0.001);
+    }
+
+    private List<Boolean[]> setFromString(String setData) {
+        final String whitespaceRegex = "\\s+";
+        List<Boolean[]> result = new LinkedList<>();
+        String[] lines = setData.split("\\n");
+
+        for (String line : lines) {
+            String[] words = line.split(whitespaceRegex);
+            Boolean[] row = new Boolean[words.length];
+            for (int i = 0; i < words.length; ++i) {
+                row[i] = words[i].equals("1");
+            }
+            result.add(row);
+        }
+
+        return result;
     }
 }
